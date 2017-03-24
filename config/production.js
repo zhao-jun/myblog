a5be0ce34c24891a2fb05f79817b4f0ece63e48f
zhaojun <443042468@qq.com>
2017-03-25 02:40:28 +0800

    test

---
+++ config/production.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongodb: 'mongodb://blog:mlab615@ds133670.mlab.com:33670/myblog'
+};
--- index.js
+++ index.js
@@ -57,10 +57,10 @@ app.use(session({
 app.use(flash());
 
 // 处理表单及文件上传的中间件
-/*app.use(require('express-formidable')({
+app.use(require('express-formidable')({
     uploadDir: path.join(__dirname, 'public/img'),// 上传文件目录
     keepExtensions: true// 保留后缀
-}));*/
+}));
 
 // 设置模板全局常量
 // app.locals.blog = {
